00ae5ab5524a78897f7f2ebe1fee8d055fbd299f
Daniil Shevtsov <me@daniilshevtsov.com>
2022-12-24 21:26:07 +0300

    Print to file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,23 @@
-import tuple.practice.runTuplePractice
+import canvas.canvas
+import canvas.color.color
+import canvas.toPmm
+import java.io.File
 
 fun main(args: Array<String>) {
-    runTuplePractice()
+    File("test.pmm").printWriter().use { out ->
+        out.println(canvas(width = 100, height = 100).let {
+            it.copy(
+                pixels = it.pixels.mapIndexed { y, row ->
+                    row.mapIndexed { x, color ->
+                        val yFraction = 0.01
+                        color(
+                            red = 1.0 * (1 - yFraction * y),
+                            green = 1.0 * (1 - yFraction * y),
+                            blue = 1.0 * (1 - yFraction * y),
+                        )
+                    }
+                }
+            ).toPmm()
+        })
+    }
 }
