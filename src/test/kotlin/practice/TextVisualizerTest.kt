9fc343a67cb5e81b3448938210f4ff39a632753a
Daniil Shevtsov <me@daniilshevtsov.com>
2022-12-24 14:19:53 +0300

    tdd

--- src/test/kotlin/practice/TextVisualizerTest.kt
+++ src/test/kotlin/practice/TextVisualizerTest.kt
@@ -7,20 +7,34 @@ import org.junit.jupiter.api.Test
 class TextVisualizerTest {
 
     @Test
-    fun `should draw field`() {
+    fun `should draw 2x2 field`() {
+        val field = visualize(size = 2, default = '*')
+        assertThat(field).isEqualTo(
+            """**
+            |**
+        """.trimMargin()
+        )
+
+    }
+
+    @Test
+    fun `should draw 3x3 field`() {
         val field = visualize(size = 3, default = '*')
-        assertThat(field).isEqualTo("""***
+        assertThat(field).isEqualTo(
+            """***
             |***
             |***
-        """.trimMargin())
+        """.trimMargin()
+        )
 
     }
 
     private fun visualize(size: Int, default: Char): String {
-        return """***
-            |***
-            |***
-        """.trimMargin()
+        return (0 until size).joinToString(separator = "\n") {
+            (0 until size).joinToString(separator = "") {
+                "*"
+            }
+        }
     }
 
 }
