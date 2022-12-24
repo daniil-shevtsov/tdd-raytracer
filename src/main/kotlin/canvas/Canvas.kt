f57954eb05c3499b30ec7fff9c4b96c69d020a34
Daniil Shevtsov <me@daniilshevtsov.com>
2022-12-24 17:17:18 +0300

    Add writing and readingh pixels

--- src/main/kotlin/canvas/Canvas.kt
+++ src/main/kotlin/canvas/Canvas.kt
@@ -1,13 +1,42 @@
 package canvas
 
 import canvas.color.Color
+import canvas.color.color
+
+data class Pixel(
+    val x: Int,
+    val y: Int,
+)
 
 data class Canvas(
     val width: Int,
     val height: Int,
-) {
-    val pixels: List<Color>
-        get() = emptyList()
+    val pixels: List<List<Color>> = (0 until height).map {
+        (0 until width).map {
+            color(0.0, 0.0, 0.0)
+        }
+    },
+)
+
+fun Canvas.writePixel(
+    x: Int,
+    y: Int,
+    color: Color
+): Canvas {
+    return copy(
+        pixels = pixels.mapIndexed { row, colorRow ->
+            colorRow.mapIndexed { column, currentColor ->
+                when {
+                    row == y && column == x -> color
+                    else -> currentColor
+                }
+            }
+        }
+    )
+}
+
+fun Canvas.pixelAt(x: Int, y: Int): Color {
+    return pixels[y][x]
 }
 
 fun canvas(
--- src/test/kotlin/canvas/CanvasTest.kt
+++ src/test/kotlin/canvas/CanvasTest.kt
@@ -3,6 +3,7 @@ package canvas
 import assertk.all
 import assertk.assertThat
 import assertk.assertions.each
+import assertk.assertions.index
 import assertk.assertions.isEqualTo
 import assertk.assertions.prop
 import canvas.color.color
@@ -17,10 +18,45 @@ internal class CanvasTest {
         assertThat(canvas).all {
             prop(Canvas::width).isEqualTo(10)
             prop(Canvas::height).isEqualTo(20)
-            prop(Canvas::pixels).each { color ->
-                color.isEqualTo(color(0.0, 0.0, 0.0))
+            prop(Canvas::pixels).all {
+                each { colorRow ->
+                    colorRow.each { color ->
+                        color.isEqualTo(color(0.0, 0.0, 0.0))
+                    }
+                }
             }
         }
+    }
+
+    @Test
+    fun `should draw pixel at canvas`() {
+        val canvas = canvas(10, 20)
+        val red = color(1.0, 0.0, 0.0)
+        val updatedCanvas = canvas.writePixel(
+            x = 2,
+            y = 3,
+            color = red,
+        )
+
+        assertThat(updatedCanvas)
+            .prop(Canvas::pixels)
+            .index(3)
+            .index(2)
+            .isEqualTo(red)
+    }
+
+    @Test
+    fun `should reada pixel from canvas`() {
+        val canvas = canvas(10, 20)
+        val red = color(1.0, 0.0, 0.0)
+        val updatedCanvas = canvas.writePixel(
+            x = 2,
+            y = 3,
+            color = red,
+        )
+
+        assertThat(updatedCanvas.pixelAt(x = 2, y = 3))
+            .isEqualTo(red)
     }
 
 }
