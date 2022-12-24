53e697333d10f58827678f89023d77b6ecce1ce1
Daniil Shevtsov <me@daniilshevtsov.com>
2022-12-24 16:52:03 +0300

    Add color operations

---
+++ src/main/kotlin/canvas/color/Color.kt
@@ -0,0 +1,70 @@
+package canvas.color
+
+import kotlin.math.abs
+
+data class Color(
+    val red: Double,
+    val green: Double,
+    val blue: Double,
+) {
+    //    private val tuple = point(
+//        x = red,
+//        y = green,
+//        z = blue,
+//    )
+//
+//    val red: Double
+//        get() = tuple.x
+//    val green: Double
+//        get() = tuple.y
+//    val blue: Double
+//        get() = tuple.z
+
+    override fun equals(other: Any?): Boolean {
+
+        return when (other) {
+            is Color -> abs(red - other.red) < EPSILON
+                    && abs(green - other.green) < EPSILON
+                    && abs(blue - other.blue) < EPSILON
+            else -> super.equals(other)
+        }
+    }
+
+    operator fun plus(other: Color): Color {
+        return Color(
+            red = red + other.red,
+            green = green + other.green,
+            blue = blue + other.blue,
+        )
+    }
+
+    operator fun minus(other: Color): Color {
+        return Color(
+            red = red - other.red,
+            green = green - other.green,
+            blue = blue - other.blue,
+        )
+    }
+
+    operator fun times(n: Double): Color {
+        return Color(
+            red = red * n,
+            green = green * n,
+            blue = blue * n,
+        )
+    }
+
+    private companion object {
+        const val EPSILON = 0.00001
+    }
+}
+
+fun color(
+    red: Double,
+    green: Double,
+    blue: Double
+) = Color(
+    red = red,
+    green = green,
+    blue = blue,
+)
---
+++ src/test/kotlin/canvas/color/ColorTest.kt
@@ -0,0 +1,42 @@
+package canvas.color
+
+import assertk.all
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.prop
+import org.junit.jupiter.api.Test
+
+internal class ColorTest {
+
+    @Test
+    fun `should create color`() {
+        val color = color(-0.5, 0.4, 1.7)
+        assertThat(color).all {
+            prop(Color::red).isEqualTo(-0.5)
+            prop(Color::green).isEqualTo(0.4)
+            prop(Color::blue).isEqualTo(1.7)
+        }
+    }
+
+    @Test
+    fun `should add colors`() {
+        val a = color(0.9, 0.6, 0.75)
+        val b = color(0.7, 0.1, 0.25)
+        assertThat(a + b).isEqualTo(color(1.6, 0.7, 1.0))
+    }
+
+    @Test
+    fun `should subtract colors`() {
+        val a = color(0.9, 0.6, 0.75)
+        val b = color(0.7, 0.1, 0.25)
+        assertThat(a - b).isEqualTo(color(0.2, 0.5, 0.5))
+    }
+
+    @Test
+    fun `should multiply color by a scalar`() {
+        val color = color(0.2, 0.3, 0.4)
+
+        assertThat(color * 2.0).isEqualTo(color(0.4, 0.6, 0.8))
+    }
+
+}
