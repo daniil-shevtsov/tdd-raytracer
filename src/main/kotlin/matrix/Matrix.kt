7793010e2ba1e9ee2766eddc1ddbfbccc9f9df71
Daniil Shevtsov <me@daniilshevtsov.com>
2022-12-24 21:48:29 +0300

    Fix matrix comparison

--- src/main/kotlin/matrix/Matrix.kt
+++ src/main/kotlin/matrix/Matrix.kt
@@ -1,9 +1,29 @@
 package matrix
 
+import kotlin.math.abs
+
 class Matrix(
     val values: List<List<Double>>,
 ) {
     operator fun get(row: Int, column: Int): Double {
         return values[row][column]
     }
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is Matrix -> values.size == other.values.size &&
+                    values.indices.all { row ->
+                        values.indices.all { column ->
+                            abs(values[row][column] - other.values[row][column]) < EPSILON
+                        }
+                    }
+            else -> super.equals(other)
+        }
+    }
+
+    private companion object {
+        const val EPSILON = 0.00001
+    }
 }
+
+fun matrix(values: List<List<Double>>) = Matrix(values = values)
--- src/test/kotlin/matrix/MatrixTest.kt
+++ src/test/kotlin/matrix/MatrixTest.kt
@@ -2,13 +2,14 @@ package matrix
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isNotEqualTo
 import org.junit.jupiter.api.Test
 
 internal class MatrixTest {
 
     @Test
     fun `should construct 4x4 matrix`() {
-        val matrix = Matrix(
+        val matrix = matrix(
             listOf(
                 listOf(1.0, 2.0, 3.0, 4.0),
                 listOf(5.5, 6.5, 7.5, 8.5),
@@ -25,4 +26,119 @@ internal class MatrixTest {
         assertThat(matrix[3, 2]).isEqualTo(15.5)
     }
 
+    @Test
+    fun `should create 2x2 matrix`() {
+        val matrix = matrix(
+            listOf(
+                listOf(-3.0, 5.0),
+                listOf(1.0, -2.0),
+            )
+        )
+        assertThat(matrix[0, 0]).isEqualTo(-3.0)
+        assertThat(matrix[0, 1]).isEqualTo(5.0)
+        assertThat(matrix[1, 0]).isEqualTo(1.0)
+        assertThat(matrix[1, 1]).isEqualTo(-2.0)
+    }
+
+    @Test
+    fun `should create 3x3 matrix`() {
+        val matrix = matrix(
+            listOf(
+                listOf(-3.0, 5.0, 0.0),
+                listOf(1.0, -2.0, -7.0),
+                listOf(0.0, 1.0, 1.0),
+            )
+        )
+        assertThat(matrix[0, 0]).isEqualTo(-3.0)
+        assertThat(matrix[1, 1]).isEqualTo(-2.0)
+        assertThat(matrix[2, 2]).isEqualTo(1.0)
+    }
+
+    @Test
+    fun `should compare equal matrices`() {
+        val a = matrix(
+            listOf(
+                listOf(1.0, 2.0, 3.0, 4.0),
+                listOf(5.5, 6.5, 7.5, 8.5),
+                listOf(9.0, 10.0, 11.0, 12.0),
+                listOf(13.5, 14.5, 15.5, 16.5)
+            )
+        )
+        val b = matrix(
+            listOf(
+                listOf(1.0, 2.0, 3.0, 4.0),
+                listOf(5.5, 6.5, 7.5, 8.5),
+                listOf(9.0, 10.0, 11.0, 12.0),
+                listOf(13.5, 14.5, 15.5, 16.5)
+            )
+        )
+
+        assertThat(a).isEqualTo(b)
+    }
+
+    @Test
+    fun `should ignore epsilon when comparing matrices`() {
+        val a = matrix(
+            listOf(
+                listOf(1.00001, 2.0, 3.0, 4.0),
+                listOf(5.5, 6.5, 7.5, 8.5),
+                listOf(9.0, 10.0, 11.0, 12.0),
+                listOf(13.5, 14.5, 15.5, 16.5)
+            )
+        )
+        val b = matrix(
+            listOf(
+                listOf(1.00002, 2.0, 3.0, 4.0),
+                listOf(5.5, 6.5, 7.5, 8.5),
+                listOf(9.0, 10.0, 11.0, 12.0),
+                listOf(13.5, 14.5, 15.5, 16.5)
+            )
+        )
+
+        assertThat(a).isEqualTo(b)
+    }
+
+    @Test
+    fun `should consider difference more than epsilon when comparing matrices`() {
+        val a = matrix(
+            listOf(
+                listOf(1.0001, 2.0, 3.0, 4.0),
+                listOf(5.5, 6.5, 7.5, 8.5),
+                listOf(9.0, 10.0, 11.0, 12.0),
+                listOf(13.5, 14.5, 15.5, 16.5)
+            )
+        )
+        val b = matrix(
+            listOf(
+                listOf(1.0002, 2.0, 3.0, 4.0),
+                listOf(5.5, 6.5, 7.5, 8.5),
+                listOf(9.0, 10.0, 11.0, 12.0),
+                listOf(13.5, 14.5, 15.5, 16.5)
+            )
+        )
+
+        assertThat(a).isNotEqualTo(b)
+    }
+
+    @Test
+    fun `should compare not equal matrices`() {
+        val a = matrix(
+            listOf(
+                listOf(1.0, 2.0, 3.0, 4.0),
+                listOf(5.5, 6.5, 7.5, 8.5),
+                listOf(9.0, 10.0, 11.0, 12.0),
+                listOf(13.5, 14.5, 15.5, 16.5)
+            )
+        )
+        val b = matrix(
+            listOf(
+                listOf(1.0, 2.0, 3.0, 4.0),
+                listOf(4.5, 6.5, 7.5, 8.5),
+                listOf(9.0, 9.0, 11.0, 12.0),
+                listOf(13.5, 14.5, 11.5, 16.5)
+            )
+        )
+
+        assertThat(a).isNotEqualTo(b)
+    }
 }
