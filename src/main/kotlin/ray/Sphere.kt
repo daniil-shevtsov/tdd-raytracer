dffd313f62e0f8b905aa91cd77a3c8096b335ac5
Daniil Shevtsov <me@daniilshevtsov.com>
2022-12-26 21:56:32 +0300

    Make normal work for transformed spheres

--- src/main/kotlin/ray/Sphere.kt
+++ src/main/kotlin/ray/Sphere.kt
@@ -2,12 +2,24 @@ package ray
 
 import matrix.Matrix
 import matrix.identityMatrix
+import tuple.Point
+import tuple.Vector
+import tuple.point
 
 data class Sphere(override val transform: Matrix = identityMatrix()) : Intersectable {
     fun transformBy(transform: Matrix): Sphere {
         return copy(
             transform = transform
         )
+    }
+    fun normalAt(worldPoint: Point): Vector {
+        val objectPoint = transform.inversed() * worldPoint
+        val objectNormal = objectPoint - point(0.0,0.0,0.0)
+
+        val worldNormal = (transform.inversed().transposed() * objectNormal)
+            .copy(w = 0.0)
+
+        return worldNormal.normalized
     }
 }
 
--- src/test/kotlin/ray/SphereTest.kt
+++ src/test/kotlin/ray/SphereTest.kt
@@ -6,10 +6,12 @@ import assertk.assertions.isEqualTo
 import assertk.assertions.prop
 import matrix.identityMatrix
 import org.junit.jupiter.api.Test
+import transformation.rotationZ
 import transformation.scaling
 import transformation.translation
 import tuple.point
 import tuple.vector
+import kotlin.math.sqrt
 
 internal class SphereTest {
 
@@ -56,4 +58,51 @@ internal class SphereTest {
         assertThat(xs).isEmpty()
     }
 
+    @Test
+    fun `normal on a sphere at point on x axis`() {
+        val normal = sphere().normalAt(point(1.0, 0.0, 0.0))
+        assertThat(normal).isEqualTo(vector(1.0, 0.0, 0.0))
+    }
+
+    @Test
+    fun `normal on a sphere at point on y axis`() {
+        val normal = sphere().normalAt(point(0.0, 1.0, 0.0))
+        assertThat(normal).isEqualTo(vector(0.0, 1.0, 0.0))
+    }
+
+    @Test
+    fun `normal on a sphere at point on z axis`() {
+        val normal = sphere().normalAt(point(0.0, 0.0, 1.0))
+        assertThat(normal).isEqualTo(vector(0.0, 0.0, 1.0))
+    }
+
+    @Test
+    fun `normal on a sphere at nonaxial point`() {
+        val fraction = sqrt(3.0) / 3.0
+        val normal = sphere().normalAt(point(fraction, fraction, fraction))
+        assertThat(normal).isEqualTo(vector(fraction, fraction, fraction))
+    }
+
+    @Test
+    fun `normal is normalized vector`() {
+        val fraction = sqrt(3.0) / 3.0
+        val normal = sphere().normalAt(point(fraction, fraction, fraction))
+        assertThat(normal).isEqualTo(normal.normalized)
+    }
+
+    @Test
+    fun `normal of a translated sphere`() {
+        val sphere = sphere()
+        val transformed = sphere.transformBy(translation(0.0, 1.0, 0.0))
+        assertThat(transformed.normalAt(point(0.0, 1.70711, -0.70711)))
+            .isEqualTo(vector(0.0, 0.70711, -0.70711))
+    }
+
+    @Test
+    fun `normal of a transformed sphere`() {
+        val sphere = sphere()
+        val transformed = sphere.transformBy(scaling(1.0, 0.5, 1.0) * rotationZ(Math.PI / 5))
+        assertThat(transformed.normalAt(point(0.0, sqrt(2.0)/2, -sqrt(2.0)/2)))
+            .isEqualTo(vector(0.0, 0.97014, -0.24254))
+    }
 }
