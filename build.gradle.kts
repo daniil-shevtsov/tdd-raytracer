3726f8975ca520ad49c4a2b07cfd7b265564e89d
Daniil Shevtsov <me@daniilshevtsov.com>
2022-12-29 00:22:03 +0300

    Add compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.20"
+    id("org.jetbrains.compose") version "1.2.2"
     application
 }
 
@@ -10,12 +11,21 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
+    implementation(compose.desktop.currentOs)
+
     testImplementation(kotlin("test"))
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
 }
+
+//compose.desktop {
+//    application {
+//        mainClass = "MainKt"
+//    }
+//}
 
 tasks.test {
     useJUnitPlatform()
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
-import file.createPmm
-import ray.practice.litSphereOnCanvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.application
 
 
-fun main(args: Array<String>) {
-    createPmm(name = "lit-sphere.pmm", content = litSphereOnCanvas())
+fun main(args: Array<String>) =  application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = WindowState(width = 400.dp, height = 300.dp),
+        title = "Ray Tracer"
+    ) {
+        Box(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
+            Text(
+                text = "Hello World",
+                fontSize = 30.sp,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
 }
