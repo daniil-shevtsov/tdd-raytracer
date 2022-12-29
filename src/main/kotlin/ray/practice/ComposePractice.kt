83b3d460c4bfb99af82d26728878bc59d82d19ed
Daniil Shevtsov <me@daniilshevtsov.com>
2022-12-29 09:55:00 +0300

    Add random color

--- src/main/kotlin/ray/practice/ComposePractice.kt
+++ src/main/kotlin/ray/practice/ComposePractice.kt
@@ -24,6 +24,7 @@ import tuple.Point
 import tuple.point
 import tuple.vector
 import java.lang.Float.max
+import kotlin.random.Random
 import androidx.compose.foundation.Canvas as ComposeCanvas
 import androidx.compose.ui.graphics.Color as ComposeColor
 
@@ -34,34 +35,42 @@ fun ComposePractice(
 ) {
     var lightPosition by remember { mutableStateOf(point(-10, 10, -10)) }
     var rayOrigin by remember { mutableStateOf(point(0.0, 0.0, -5.0)) }
-    var color by remember { mutableStateOf(color(0.0, 0.0, -5.0)) }
+    var color by remember { mutableStateOf(color(1.0, 0.2, 1.0)) }
     val requester = remember { FocusRequester() }
     Column {
         MyCanvas(
-            canvas = controlledLitSpherePractice(lightPosition, rayOrigin),
+            canvas = controlledLitSpherePractice(lightPosition, rayOrigin, color),
             modifier.onKeyEvent {
-                    val step = 0.1
-                    val direction = vector(
-                        x = when (it.key) {
-                            Key.A -> -1.0
-                            Key.D -> 1.0
-                            else -> 0.0
-                        },
-                        y = when (it.key) {
-                            Key.W -> -1.0
-                            Key.S -> 1.0
-                            else -> 0.0
-                        },
-                        z = 0.0,
+                val step = 0.1
+                val direction = vector(
+                    x = when (it.key) {
+                        Key.A -> -1.0
+                        Key.D -> 1.0
+                        else -> 0.0
+                    },
+                    y = when (it.key) {
+                        Key.W -> -1.0
+                        Key.S -> 1.0
+                        else -> 0.0
+                    },
+                    z = 0.0,
+                )
+                val isCamera = it.isShiftPressed
+                color = when (it.key) {
+                    Key.Spacebar -> color(
+                        Random.nextDouble(0.0, 1.0),
+                        Random.nextDouble(0.0, 1.0),
+                        Random.nextDouble(0.0, 1.0),
                     )
-                    val isCamera = it.isShiftPressed
-                    if (isCamera) {
-                        rayOrigin = rayOrigin + direction * step
-                    } else {
-                        lightPosition = lightPosition + direction * step * 3.0
-                    }
-                    direction.components.any { it != 0.0 }
+                    else -> color
                 }
+                if (isCamera) {
+                    rayOrigin = rayOrigin + direction * step
+                } else {
+                    lightPosition = lightPosition + direction * step * 3.0
+                }
+                direction.components.any { it != 0.0 } || it.key == Key.Spacebar
+            }
                 .focusRequester(requester)
                 .focusable()
         )
@@ -95,13 +104,14 @@ fun MyCanvas(canvas: Canvas, modifier: Modifier = Modifier) {
 fun controlledLitSpherePractice(
     lightPosition: Point = point(-10, 10, -10),
     rayOrigin: Point = point(0.0, 0.0, -5.0),
+    color: Color = color(1.0, 0.2, 1.0)
 ): Canvas {
     val canvasPixels = 100
     val wallZ = 10.0
     val wallSize = 7.0
     val pixelSize = wallSize / canvasPixels
     val half = wallSize / 2.0
-    val sphere = sphere(material = material(color = color(1.0, 0.2, 1.0)))
+    val sphere = sphere(material = material(color = color))
 
     val light = pointLight(
         position = lightPosition,
